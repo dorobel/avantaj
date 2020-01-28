96c6cfd6fc8c4511851b53d55557f928ab8cf697
dorobel <dorobantun@yahoo.com>
2020-01-28 14:52:00 +0100

    28/1

--- administration/forms.py
+++ administration/forms.py
@@ -46,32 +46,34 @@ class SearchForm(forms.Form):
     cnp_nbr = forms.IntegerField(label='CNP', widget=forms.TextInput(attrs={'class': 'form-control' }), required=False)  
     first_name = forms.CharField(label='First Name', widget=forms.TextInput(attrs={'class': 'form-control'}),  required=False)  
     last_name = forms.CharField(label='Last Name', widget=forms.TextInput(attrs={'class': 'form-control'}),  required=False)
- 
-    def clean(self):  # most used!!!
-        all_clean_data = super().clean()
-        cnp_nbr = all_clean_data['cnp_nbr']
-        first_name = all_clean_data['first_name']
-        last_name = all_clean_data['last_name']
-
-        if not cnp_nbr or ( not first_name and not last_name):
-            raise forms.ValidationError("Enter f23")
-        return all_clean_data
-            
+    
     class Meta():
         model = Clients                                
         fields = ('cnp_nbr','first_name','last_name')
-    
-
-            
-'''
+        
     def clean(self):  # most used!!!
         all_clean_data = super().clean()
-        cnp_nbr = all_clean_data['cnp_nbr']
-        first_name = all_clean_data['first_name']
-        last_name = all_clean_data['last_name']
+        cnp_nbr = all_clean_data.get('cnp_nbr')
+        first_name = all_clean_data.get('first_name')
+        last_name = all_clean_data.get('last_name')
 
-        if not cnp_nbr or not (first_name and last_name):
-            raise forms.ValidationError("Enter f1 or f2&f3")
+        if  cnp_nbr is None and (first_name =='' or last_name ==''): #That's because a not required IntegerField will be None if it's empty, whereas a not required CharField will be '' (the empty string) if it's empty.
+            raise forms.ValidationError("Enter either CNP either first_name&last_name")
+            super().clean()
+            
+                # Boolean OR operator returns true if any one operand is true
+                # Boolean AND operator returns true if both operands return true
+                # The NOT operator returns true if its operand is a false expression and returns false if it is true.
+                
+                # DC cnp_nbr este null (TRUE)  si conditia ( first_name =='' or last_name =='' ) intoarce TRUE       ==> TRUE => Validation Error
+                
+                # Dc cnp_nbr are valoare (FALSE)  si conditia ( first_name =='' or last_name =='' ) intoarce TRUE    ==> FALSE => NO Validation error
+                
+                # DC cnp_nbr este null (TRUE) si conditia ( first_name =='' or last_name =='' ) intoarce FALSE       ==> FALSE => NO Validation error
+                
+                # DC cnp_nbr are valoare (FALSE) si conditia ( first_name =='' or last_name =='' ) intoarce FALSE    ==> FALSE => NO Validation error
+                
+        
+            
+   
     
-        return all_clean_data
-'''
